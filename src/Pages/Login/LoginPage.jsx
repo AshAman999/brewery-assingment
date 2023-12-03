27ee7a1d4248729b4898f764419216a8f3279d21
Aman Raj <amanpie9@gmail.com>
2023-12-03 18:37:37 +0530

    Add form submission functionality to Login and SignUp pages

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Make API call to sign in
+    try {
+      const response = await fetch("/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        // Store the token in localStorage
+        localStorage.setItem("token", data.token);
+
+        // You can redirect to another page or perform other actions after successful login
+        console.log("Login successful!");
+      } else {
+        console.error("Login failed");
+        // Handle unsuccessful login, show an error message, etc.
+      }
+    } catch (error) {
+      console.error("Error during login:", error.message);
+    }
+  };
+
   return (
     <Container
       className="justify-content-center align-items-center"
       style={{ maxWidth: "60%" }}>
       <h1>Login</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
--- src/Pages/SignUpPage/SignUpPage.jsx
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Form, Button } from "react-bootstrap";
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate password and confirm password
+    if (password !== confirmPassword) {
+      console.error("Passwords do not match");
+      return;
+    }
+
+    // Make API call to sign up
+    try {
+      const response = await fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // You can redirect to another page or perform other actions after successful sign-up
+        console.log("Sign-up successful!");
+      } else {
+        console.error("Sign-up failed");
+        // Handle unsuccessful sign-up, show an error message, etc.
+      }
+    } catch (error) {
+      console.error("Error during sign-up:", error.message);
+    }
+  };
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -10,20 +45,35 @@ const SignUpPage = () => {
       <Row>
         <Col>
           <h1>Sign Up</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
+            <Form.Group controlId="formBasicConfirmPassword">
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Confirm Password" />
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
             </Form.Group>
 
             <Button variant="primary" type="submit">
