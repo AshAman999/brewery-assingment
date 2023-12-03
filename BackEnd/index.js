1bf8a5076b98aa4c6542518d583b05468b3aaa1f
Aman Raj <amanpie9@gmail.com>
2023-12-03 14:59:50 +0530

    basic backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,64 +1,138 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/brewery', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Failed to connect to MongoDB', err));
-
-// Define Brewery schema
-const brewerySchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  ratingCount: { type: Number, default: 0 },
-  totalRating: { type: Number, default: 0 }
+// MySQL connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "your_username",
+  password: "your_password",
+  database: "your_database",
 });
 
-const Brewery = mongoose.model('Brewery', brewerySchema);
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to MySQL");
+});
 
-// Add rating for a brewery
-app.post('/breweries/:id/rating', async (req, res) => {
-  const { id } = req.params;
-  const { rating } = req.body;
+app.use(express.json());
 
-  try {
-    const brewery = await Brewery.findOne({ id });
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    if (!brewery) {
-      return res.status(404).json({ message: 'Brewery not found' });
+  jwt.verify(token, "your_secret_key", (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+// Sign up route
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Insert user into the database
+  const query = "INSERT INTO users (email, password) VALUES (?, ?)";
+  db.query(query, [email, hashedPassword], (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Error creating user" });
     }
 
-    brewery.ratingCount++;
-    brewery.totalRating += rating;
-    await brewery.save();
-
-    res.json({ message: 'Rating added successfully' });
-  } catch (error) {
-    console.error('Failed to add rating', error);
-    res.status(500).json({ message: 'Failed to add rating' });
-  }
+    res.status(201).json({ message: "User created successfully" });
+  });
 });
 
-// Get average rating for a brewery
-app.get('/breweries/:id/average-rating', async (req, res) => {
-  const { id } = req.params;
+// Sign in route
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
 
-  try {
-    const brewery = await Brewery.findOne({ id });
-
-    if (!brewery) {
-      return res.json({ averageRating: 0 });
+  // Check if user exists
+  const query = "SELECT * FROM users WHERE email = ?";
+  db.query(query, [email], async (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Error checking user" });
     }
 
-    const averageRating = brewery.totalRating / brewery.ratingCount;
-    res.json({ averageRating });
-  } catch (error) {
-    console.error('Failed to get average rating', error);
-    res.status(500).json({ message: 'Failed to get average rating' });
-  }
+    if (result.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Check password
+    const match = await bcrypt.compare(password, result[0].password);
+    if (!match) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Create and return JWT token
+    const token = jwt.sign({ email }, "your_secret_key");
+    res.json({ token });
+  });
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+// Get brewery rating and reviews route
+app.get("/brewery/:id", verifyToken, (req, res) => {
+  const breweryId = req.params.id;
+
+  // Query to get average rating and reviews for a brewery
+  const query =
+    "SELECT AVG(rating) AS avgRating, COUNT(*) AS reviewCount FROM ratings WHERE brewery_id = ?";
+  db.query(query, [breweryId], (err, result) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "Error fetching brewery rating and reviews" });
+    }
+
+    const { avgRating, reviewCount } = result[0];
+    res.json({ avgRating, reviewCount });
+  });
+});
+
+// Post a rating and comment route
+app.post("/rating", verifyToken, (req, res) => {
+  const { breweryId, rating, comment } = req.body;
+
+  const query =
+    "INSERT INTO ratings (brewery_id, rating, comment) VALUES (?, ?, ?)";
+  db.query(query, [breweryId, rating, comment], (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Error posting rating" });
+    }
+
+    res.status(201).json({ message: "Rating posted successfully" });
+  });
+});
+
+// Edit a rating and comment route
+app.put("/rating/:id", verifyToken, (req, res) => {
+  const ratingId = req.params.id;
+  const { rating, comment } = req.body;
+
+  const query = "UPDATE ratings SET rating = ?, comment = ? WHERE id = ?";
+  db.query(query, [rating, comment, ratingId], (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Error updating rating" });
+    }
+
+    res.json({ message: "Rating updated successfully" });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
