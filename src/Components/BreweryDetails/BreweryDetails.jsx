5101316ee93a2f15a2c8d2b9423a0d7c4d0e8adf
Aman Raj <amanpie9@gmail.com>
2023-12-05 15:21:55 +0530

    mvp done

--- src/Components/BreweryDetails/BreweryDetails.jsx
+++ src/Components/BreweryDetails/BreweryDetails.jsx
@@ -4,7 +4,6 @@ import { useLocation } from "react-router-dom";
 const BreweryDetails = () => {
   const [brewery, setBrewery] = useState({});
   const [ratings, setRatings] = useState([]);
-  const [userRating, setUserRating] = useState(null);
   const location = useLocation();
 
   const queryParams = new URLSearchParams(location.search);
@@ -18,112 +17,92 @@ const BreweryDetails = () => {
   const [editModeList, setEditModeList] = useState([]);
   const [alreadyRated, setAlreadyRated] = useState(false);
 
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: localStorage.getItem("token"),
+  };
+
+  const fetchData = async (url, options = {}) => {
+    const response = await fetch(url, { headers, ...options });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  };
+
+  const updateRatingsAndEditModeList = (data) => {
+    setRatings(data.ratings);
+    setRating(data.avgRating);
+    setAlreadyRated(checkIfUserHasRated(data.ratings));
+    setEditModeList(data.ratings.map(() => false));
+  };
+
+  const checkIfUserHasRated = (ratings) =>
+    ratings.some(
+      (rating) => rating.user_email === localStorage.getItem("email")
+    );
+
   const getDetails = async () => {
-    fetch(`https://api.openbrewerydb.org/v1/breweries/${id}`)
-      .then((response) => response.json())
-      .then((data) => setBrewery(data));
+    const data = await fetchData(
+      `https://api.openbrewerydb.org/v1/breweries/${id}`
+    );
+    setBrewery(data);
 
-    fetch(`http://localhost:4000/brewery/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: localStorage.getItem("token"),
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setRatings(data.ratings);
-        setRating(data.avgRating);
-        // Check if the user has already rated
-        const hasUserRated = data.ratings.some(
-          (rating) => rating.user_email === localStorage.getItem("email")
-        );
-        setAlreadyRated(hasUserRated);
-
-        // Initialize edit mode list
-        setEditModeList(data.ratings.map(() => false));
-      });
+    const data2 = await fetchData(`http://localhost:4000/brewery/${id}`);
+    updateRatingsAndEditModeList(data2);
   };
 
   useEffect(() => {
     getDetails();
-  }, [id, getDetails]);
+  }, [id]);
 
-  const handleRatingSubmit = (event) => {
+  const handleRatingSubmit = async (event) => {
     event.preventDefault();
 
-    fetch(`http://localhost:4000/rating`, {
+    await fetchData(`http://localhost:4000/rating`, {
       method: "POST",
       body: JSON.stringify({
         breweryId: id,
         rating: newRating,
         comment: newComment,
       }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: localStorage.getItem("token"),
-      },
-    })
-      .then((response) => response.json())
-      .then(async () => {
-        const response = await fetch(`http://localhost:4000/brewery/${id}`, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: localStorage.getItem("token"),
-          },
-        });
-        const data = await response.json();
-        setRatings(data.ratings);
-        setRating(data.avgRating);
-        setAlreadyRated(true);
-      });
+    });
+
+    const data = await fetchData(`http://localhost:4000/brewery/${id}`);
+    updateRatingsAndEditModeList(data);
   };
 
-  const handleRatingEdit = (index) => {
-    fetch(`http://localhost:4000/rating`, {
+  const handleRatingEdit = async (index) => {
+    const data = await fetchData(`http://localhost:4000/rating`, {
       method: "PUT",
       body: JSON.stringify({ rating: editRating, comment: editComment }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: localStorage.getItem("token"),
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data.message === "Rating updated successfully") {
-          // Update state to reflect the changes
-          const updatedRatings = [...ratings];
-          updatedRatings[index] = {
-            ...updatedRatings[index],
-            rating: editRating,
-            comment: editComment,
-          };
-          setRatings(updatedRatings);
+    });
 
-          // Disable edit mode for the current rating
-          const updatedEditModeList = [...editModeList];
-          updatedEditModeList[index] = false;
-          setEditModeList(updatedEditModeList);
-          getDetails();
-        } else {
-          // Handle error message from server
-          console.error(data.message);
-        }
-      })
-      .catch((error) => {
-        // Handle fetch error
-        console.error(error);
-      });
+    if (data.message === "Rating updated successfully") {
+      const updatedRatings = [...ratings];
+      updatedRatings[index] = {
+        ...updatedRatings[index],
+        rating: editRating,
+        comment: editComment,
+      };
+      setRatings(updatedRatings);
+
+      const updatedEditModeList = [...editModeList];
+      updatedEditModeList[index] = false;
+      setEditModeList(updatedEditModeList);
+
+      getDetails();
+    } else {
+      console.error(data.message);
+    }
   };
 
   const enableEditMode = (index) => {
     const updatedEditModeList = [...editModeList];
     updatedEditModeList[index] = true;
     setEditModeList(updatedEditModeList);
+    setEditRating(ratings[index].rating);
+    setEditComment(ratings[index].comment);
   };
 
   return (
@@ -137,62 +116,65 @@ const BreweryDetails = () => {
         {brewery.state}, {brewery.city}
       </p>
 
-      {ratings.map((rating, index) => (
-        <div
-          key={index}
-          style={{
-            border: "1px solid black",
-            padding: "1rem",
-            margin: "1rem",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}>
-          <p>
-            User:
-            {rating.user_email === localStorage.getItem("email")
-              ? "You"
-              : rating.user_email}
-          </p>
+      {ratings
+        .sort((a, b) =>
+          a.user_email === localStorage.getItem("email") ? -1 : 1
+        )
+        .map((rating, index) => (
+          <div
+            key={index}
+            style={{
+              border: "1px solid black",
+              padding: "1rem",
+              margin: "1rem",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}>
+            <p>
+              User:
+              {rating.user_email === localStorage.getItem("email")
+                ? "You"
+                : rating.user_email}
+            </p>
 
-          {editModeList[index] ? (
-            <div>
-              <label>Rating:</label>
-              <input
-                type="number"
-                min="1"
-                max="5"
-                step="1"
-                value={editRating}
-                onChange={(e) => setEditRating(e.target.value)}
-              />
-              <br />
-              <label>Comment:</label>
-              <input
-                type="text"
-                value={editComment}
-                onChange={(e) => setEditComment(e.target.value)}
-              />
-              <br />
-              <button onClick={() => handleRatingEdit(index)}>Save</button>
-              <button
-                onClick={() =>
-                  setEditModeList((prev) => ({ ...prev, [index]: false }))
-                }>
-                Cancel
-              </button>
-            </div>
-          ) : (
-            <div>
-              <p>Rating: {rating.rating}</p>
-              <p>Comment: {rating.comment}</p>
-              {rating.user_email === localStorage.getItem("email") && (
-                <button onClick={() => enableEditMode(index)}>Edit</button>
-              )}
-            </div>
-          )}
-        </div>
-      ))}
-
+            {editModeList[index] ? (
+              <div>
+                <label>Rating:</label>
+                <input
+                  type="number"
+                  min="1"
+                  max="5"
+                  step="1"
+                  value={editRating}
+                  onChange={(e) => setEditRating(e.target.value)}
+                />
+                <br />
+                <label>Comment:</label>
+                <input
+                  type="text"
+                  value={editComment}
+                  onChange={(e) => setEditComment(e.target.value)}
+                />
+                <br />
+                <button onClick={() => handleRatingEdit(index)}>Save</button>
+                <button
+                  onClick={() =>
+                    setEditModeList((prev) => ({ ...prev, [index]: false }))
+                  }>
+                  Cancel
+                </button>
+              </div>
+            ) : (
+              <div>
+                <p>Rating: {rating.rating}</p>
+                <p>Comment: {rating.comment}</p>
+                {rating.user_email === localStorage.getItem("email") && (
+                  <button onClick={() => enableEditMode(index)}>Edit</button>
+                )}
+              </div>
+            )}
+          </div>
+        ))}
       {!alreadyRated && (
         <form onSubmit={handleRatingSubmit}>
           <input
